f6b0517c28eaacdade31269f54a55370a004a5d4
Asko Nomm <asko@bien.ee>
2022-04-01 10:12:59 +0200

    Fix feed

--- src/feed.xml.clj
+++ src/feed.xml.clj
@@ -1,4 +1,5 @@
 (declare xml)
+(declare format-date)
 (declare content)
 
 
@@ -10,6 +11,17 @@
 
 
 ; Render page.
-(xml
+[:rss {:version    "2.0"
+       :xmlns:atom "http://www.w3.org/2005/Atom"}
+ [:channel
+  [:atom:link {:href "https://bien.ee/feed.xml" :rel "self" :type "application/rss+xml"}]
+  [:title "Bien"]
+  [:description ""]
+  [:link "https://bien.ee"]
   (for [post posts]
-    []))
+    [:item
+     [:title (:title post)]
+     [:description ""]
+     [:link (str "https://bien.ee/" (:slug post))]
+     [:guid (str "https://bien.ee/" (:slug post))]
+     [:pubDate (str (format-date (:date post) "dd MMM YYYY 00:00:00") " GMT")]])]]
