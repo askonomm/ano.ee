b8096113f179612597f9a12c7085f6c96ea204a9
Asko Nõmm <asko@bien.ee>
2022-04-16 16:22:29 +0300

    Update footer.clj
    Update address.

--- src/_partials/footer.clj
+++ src/_partials/footer.clj
@@ -1,4 +1,4 @@
 [:div.footer
- [:p "Hi there. I'm Asko Nõmm, a software engineer from Estonia, currently living in Alicante, Spain. You can check my open source work on " [:a {:href "https://github.com/askonomm"} "GitHub"] " and shoot me an e-mail to " [:a {:href "mailto:asko@bien.ee"} "asko@bien.ee"] "."]
+ [:p "Hi there. I'm Asko Nõmm, a software engineer from Tallinn, Estonia. You can check my open source work on " [:a {:href "https://github.com/askonomm"} "GitHub"] " and shoot me an e-mail to " [:a {:href "mailto:asko@bien.ee"} "asko@bien.ee"] "."]
  [:script {:src "/highlight.min.js"}]
  [:script "hljs.highlightAll();"]]
