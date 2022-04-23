97ce744c71cc4097c1aa119f1110367bbb2f70b5
Asko Nõmm <asko@bien.ee>
2022-04-24 01:58:25 +0300

    Update footer.clj

--- src/_partials/footer.clj
+++ src/_partials/footer.clj
@@ -1,4 +1,4 @@
 [:div.footer
- [:p "Hi there. I'm Asko Nõmm, a software engineer from Tallinn, Estonia. You can check my open source work on " [:a {:href "https://github.com/askonomm"} "GitHub"] " and shoot me an e-mail to " [:a {:href "mailto:ano@ano.ee"} "ano@ano.ee"] "."]
+ [:p "Hi there. I'm Asko Nõmm, a software engineer from Tallinn, Estonia, specializing in Clojure / ClojureScript. You can check my open source work on " [:a {:href "https://github.com/askonomm"} "GitHub"] " and shoot me an e-mail to " [:a {:href "mailto:ano@ano.ee"} "ano@ano.ee"] "."]
  [:script {:src "/highlight.min.js"}]
  [:script "hljs.highlightAll();"]]
